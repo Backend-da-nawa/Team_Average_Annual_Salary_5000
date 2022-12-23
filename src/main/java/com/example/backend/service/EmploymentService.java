1d5aaef6cfb83f2f06f2670b3d42e14264242277
dong yeol <ehfql6363@naver.com>
2022-12-23 14:22:56 +0900

    FEAT(EmploymentService) : Create class and Write 'load' method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -1,2 +1,22 @@
-package com.example.backend.service;public class EmploymentService {
+package com.example.backend.service;
+
+import com.example.backend.REPO.CompanyRepository;
+import com.example.backend.REPO.EmploymentRepository;
+import com.example.backend.entity.Employment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class EmploymentService {
+    private final EmploymentRepository employmentRepository;
+    private final CompanyRepository companyRepository;
+
+    @Transactional
+    public List<Employment> load() throws Exception {
+        return employmentRepository.findAll();
+    }
 }
