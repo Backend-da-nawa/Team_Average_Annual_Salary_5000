d048f10af78ec90d1727495fbab37ee7edd6bd3d
dong yeol <ehfql6363@naver.com>
2022-12-23 14:21:37 +0900

    FEAT(EmploymentRepository) : Create class and Write 'load' method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -1,2 +1,23 @@
-package com.example.backend.controller;public class EmploymentController {
+package com.example.backend.controller;
+
+import com.example.backend.entity.Employment;
+import com.example.backend.service.EmploymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class EmploymentController {
+
+    private final EmploymentService employmentService;
+
+    @GetMapping("/employ/load")
+    public List<Employment> load() throws Exception {
+        return employmentService.load();
+    }
 }
