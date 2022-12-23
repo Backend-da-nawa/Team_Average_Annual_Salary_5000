a67b4ef58187b4e37f3e64954333701d010c5444
dong yeol <ehfql6363@naver.com>
2022-12-23 13:22:56 +0900

    FEAT(EmploymentVO) : Create and Set EmploymentVO
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/VO/EmploymentVO.java
+++ src/main/java/com/example/backend/VO/EmploymentVO.java
@@ -1,2 +1,33 @@
-package com.example.backend.VO;public class EmploymentVO {
+package com.example.backend.VO;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Entity
+@Getter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "employment")
+public class EmploymentVO {
+    @Id
+    @Column(name = "emp_id")
+    private Long id;
+
+    @JoinColumn(name = "com_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private CompanyVO company;
+
+    @Column(name = "emp_position")
+    private String position;
+
+    @Column(name = "emp_compensation")
+    private Long compensation;
+
+    @Column(name = "emp_stack")
+    private String stack;
+
 }
