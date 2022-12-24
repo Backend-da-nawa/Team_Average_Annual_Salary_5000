7169ca67892d5ed423bbf22244ca94fcff1a8cb9
jojeongbin <cjb1009@naver.com>
2022-12-25 01:48:05 +0900

    REFACTOR(build.gradle):Change spring_version

---
+++ src/main/java/com/example/backend/DTO/EmploymentSubmitDTO.java
@@ -0,0 +1,40 @@
+package com.example.backend.DTO;
+
+import com.example.backend.entity.Company;
+import com.example.backend.entity.Employment;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
+@Getter
+@NoArgsConstructor
+@Data
+public class submitDTO {
+
+    private Long com_id;
+    private String com_name;
+    private String com_country;
+    private String com_area;
+    private String emp_position;
+    private Long emp_compensation;
+    private String emp_stack;
+
+    @Builder
+    public submitDTO(Company company) {
+        this.com_id = company.getId();
+        this.com_name = company.getName();
+        this.com_country = company.getCountry();
+        this.com_area = company.getArea();
+    }
+
+    @Builder
+    public submitDTO(Employment employment) {
+        this.emp_position = employment.getPosition();
+        this.emp_compensation = employment.getCompensation();
+        this.emp_stack = employment.getStack();
+    }
+
+}
---
+++ src/main/java/com/example/backend/VO/EmploymentSubmitVO.java
@@ -0,0 +1,5 @@
+package com.example.backend.VO;
+
+public class submitVO {
+    private 
+}
