e52cb3153367efb23aec5f02b1b624e948e0bd93
dong yeol <ehfql6363@naver.com>
2022-12-24 15:29:41 +0900

    FEAT(EmploymentRepository) : Add Repository annotation and query method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/REPO/EmploymentRepository.java
+++ src/main/java/com/example/backend/REPO/EmploymentRepository.java
@@ -1,7 +1,13 @@
 package com.example.backend.REPO;
 
 import com.example.backend.entity.Employment;
+import com.example.backend.mapping.WithoutContent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EmploymentRepository extends JpaRepository<Employment, Long> {
+//    List<WithoutContent> findProjection();
 }
