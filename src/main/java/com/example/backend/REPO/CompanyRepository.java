417d322ef86f2d480dbe8570ade98ce2d6d83e76
dong yeol <ehfql6363@naver.com>
2022-12-24 15:28:32 +0900

    FEAT(CompanyRepository) : Add Repository annotation
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/REPO/CompanyRepository.java
+++ src/main/java/com/example/backend/REPO/CompanyRepository.java
@@ -4,5 +4,6 @@ import com.example.backend.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
