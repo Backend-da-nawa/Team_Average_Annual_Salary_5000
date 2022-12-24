dd22fe562e96600a8d5ee5c0080b6c12b0fe1f71
dong yeol <ehfql6363@naver.com>
2022-12-24 17:04:43 +0900

    FEAT(CompanyService) : Create read method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/CompanyService.java
+++ src/main/java/com/example/backend/service/CompanyService.java
@@ -1,9 +1,25 @@
 package com.example.backend.service;
 
+import com.example.backend.REPO.CompanyRepository;
+import com.example.backend.entity.Company;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public Company read(Long id) throws Exception {
+        Optional<Company> optional = companyRepository.findById(id);
+
+        Company company;
+        if (optional.isPresent()) company = optional.get();
+        else throw new Exception();
+
+        return company;
+    }
 }
