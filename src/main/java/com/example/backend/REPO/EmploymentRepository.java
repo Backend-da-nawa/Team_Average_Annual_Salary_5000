4530f6ec183e9f128d791980271436136b4de0ab
jojeongbin <cjb1009@naver.com>
2022-12-25 02:06:26 +0900

    REFACTOR(EmploymentRepository) : Change parameter

--- src/main/java/com/example/backend/REPO/EmploymentRepository.java
+++ src/main/java/com/example/backend/REPO/EmploymentRepository.java
@@ -1,2 +1,13 @@
-package com.example.backend.REPO;public class EmploymentRepository {
+package com.example.backend.REPO;
+
+import com.example.backend.entity.Employment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmploymentRepository extends JpaRepository<Employment, Long> {
+
 }
