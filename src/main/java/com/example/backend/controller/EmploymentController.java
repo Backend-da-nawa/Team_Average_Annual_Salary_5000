dc4ac5574a72edec8c164657d4548b1e61543c4b
jojeongbin <cjb1009@naver.com>
2022-12-25 02:10:43 +0900

    REFACTOR(EmploymentController) : Change add method

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -1,2 +1,26 @@
-package com.example.backend.controller;public class EmploymentController {
+package com.example.backend.controller;
+
+import com.example.backend.VO.EmploymentSubmitVO;
+import com.example.backend.entity.Employment;
+import com.example.backend.service.EmploymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+@Controller
+public class EmploymentController {
+    private final EmploymentService employmentService;
+
+    public EmploymentController(EmploymentService boardService) {
+        this.employmentService = boardService;
+    }
+
+    @PostMapping("/employ/submit")
+    public void employSubmit(@RequestBody EmploymentSubmitVO employmentSubmitVO)throws Exception{
+        employmentService.employSubmit(employmentSubmitVO);
+    }
+
+
+
 }
