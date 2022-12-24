78828ef59ce2f0da92b5fee76e6efaa5b12085c5
jojeongbin <cjb1009@naver.com>
2022-12-25 02:12:58 +0900

    FEAT(Company) : Create class

--- src/main/java/com/example/backend/entity/Company.java
+++ src/main/java/com/example/backend/entity/Company.java
@@ -1,2 +1,36 @@
-package com.example.backend.entity;public class Company {
+package com.example.backend.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@ToString
+@Getter
+@Table(name = "company")
+@RequiredArgsConstructor
+public class Company {
+
+    @Id
+    @Column(name = "com_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "company",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Employment> employments;
+
+    @Column(name = "com_name")
+    private String name;
+
+    @Column(name = "com_country")
+    private String country;
+
+    @Column(name = "com_area")
+    private String area;
+
 }
