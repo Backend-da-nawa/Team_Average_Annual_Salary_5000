0b93729eb64cadcb9e8a69760e233d99d0762e8f
dong yeol <ehfql6363@naver.com>
2022-12-24 13:03:11 +0900

    FEAT(LoadDTO) : Create DTO class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/DTO/LoadDTO.java
+++ src/main/java/com/example/backend/DTO/LoadDTO.java
@@ -1,2 +1,22 @@
-package com.example.backend.DTO;public class LoadDTO {
+package com.example.backend.DTO;
+
+import com.example.backend.entity.Company;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoadDTO {
+
+//    private Company company;
+    private Long com_id;
+    private String com_name;
+    private String com_country;
+    private String com_area;
+    private String emp_position;
+    private Long emp_compensation;
+    private String emp_stack;
+
+
+
 }
