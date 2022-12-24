d10c975fa189209abebf9ec23cb85c9378d45453
dong yeol <ehfql6363@naver.com>
2022-12-24 15:35:52 +0900

    FEAT(EmploymentService) : Change return type and code
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -2,21 +2,24 @@ package com.example.backend.service;
 
 import com.example.backend.REPO.CompanyRepository;
 import com.example.backend.REPO.EmploymentRepository;
-import com.example.backend.entity.Employment;
+import com.example.backend.mapping.WithoutContent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class EmploymentService {
     private final EmploymentRepository employmentRepository;
     private final CompanyRepository companyRepository;
 
     @Transactional
-    public List<Employment> load() throws Exception {
-        return employmentRepository.findAll();
+    public List<WithoutContent> load() throws Exception {
+//        List<WithoutContent> employments = employmentRepository.findAllEmploymentLoadProjectionBy();
+
+//        return employments;
+        return null;
     }
 }
