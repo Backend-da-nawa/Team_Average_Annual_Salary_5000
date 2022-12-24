537dd3be3ae78bc3b7b721a09dff7a46c4248c3c
dong yeol <ehfql6363@naver.com>
2022-12-24 16:03:37 +0900

    FEAT(EmploymentController) : Change return type
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -1,10 +1,13 @@
 package com.example.backend.controller;
 
+import com.example.backend.REPO.CompanyRepository;
+import com.example.backend.entity.Company;
 import com.example.backend.mapping.WithoutContent;
 import com.example.backend.service.EmploymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
