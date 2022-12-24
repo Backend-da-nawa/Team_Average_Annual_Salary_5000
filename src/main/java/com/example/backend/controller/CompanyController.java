cef8ca66e3117341e0a719341cc34446ce3aeed9
dong yeol <ehfql6363@naver.com>
2022-12-24 16:02:10 +0900

    FEAT(CompanyController) : Create class
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/controller/CompanyController.java
+++ src/main/java/com/example/backend/controller/CompanyController.java
@@ -1,2 +1,21 @@
-package com.example.backend.controller;public class CompanyController {
+package com.example.backend.controller;
+
+import com.example.backend.entity.Company;
+import com.example.backend.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class CompanyController {
+    private final CompanyService companyService;
+
+    @GetMapping("/employ/load")
+    public Company read(@RequestParam long id) throws Exception {
+        return
+    }
 }
