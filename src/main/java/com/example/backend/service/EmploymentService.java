067222cd9c948f87d8435084393b4abb7d56ee2b
dong yeol <ehfql6363@naver.com>
2022-12-25 11:24:37 +0900

    FEAT(EmploymentService) : Change object
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.LoadDTO;
 import com.example.backend.REPO.CompanyRepository;
 import com.example.backend.REPO.EmploymentRepository;
 import com.example.backend.entity.Company;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +18,16 @@ public class EmploymentService {
     private final EmploymentRepository employmentRepository;
 
     @Transactional
-    public List<WithoutContent> load() throws Exception {
-        return employmentRepository.findAllBy();
+    public List<LoadDTO> load() throws Exception {
+        List<WithoutContent> mapping = employmentRepository.findAllBy();
+
+        List<LoadDTO> loadDTOList = new ArrayList<>();
+
+        for (int i = 0; i < mapping.size(); i++) {
+            LoadDTO loadDTO = new LoadDTO(mapping.get(i));
+            loadDTOList.add(loadDTO);
+        }
+
+        return loadDTOList;
     }
 }
