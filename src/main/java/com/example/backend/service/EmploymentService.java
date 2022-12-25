3a932b48df92f7dde65b23adc552c3335eb82ad0
dong yeol <ehfql6363@naver.com>
2022-12-25 21:16:56 +0900

    FEAT(EmploymentService) : Create search service method and getting list method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -6,9 +6,10 @@ import com.example.backend.REPO.EmploymentRepository;
 import com.example.backend.entity.Company;
 import com.example.backend.mapping.WithoutContent;
 import lombok.RequiredArgsConstructor;
+import lombok.With;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,11 +24,42 @@ public class EmploymentService {
 
         List<LoadDTO> loadDTOList = new ArrayList<>();
 
-        for (int i = 0; i < mapping.size(); i++) {
-            LoadDTO loadDTO = new LoadDTO(mapping.get(i));
+        for (WithoutContent withoutContent : mapping) {
+            LoadDTO loadDTO = new LoadDTO(withoutContent);
             loadDTOList.add(loadDTO);
         }
 
         return loadDTOList;
+    }
+
+    @Transactional
+    public List<LoadDTO> search(String search) throws Exception {
+        List<LoadDTO> loadDTOList = new ArrayList<>();
+
+        companySearch(loadDTOList, search);
+
+        stackSearch(loadDTOList, search);
+
+        return loadDTOList;
+    }
+
+    private void companySearch(List<LoadDTO> loadDTOList, String search) {
+        LoadDTO loadDTO;
+        List<WithoutContent> searchWithCompanyList = employmentRepository.findAllByCompany_NameLike(search);
+
+        for (WithoutContent companySearch : searchWithCompanyList) {
+            loadDTO = new LoadDTO(companySearch);
+            loadDTOList.add(loadDTO);
+        }
+    }
+
+    private void stackSearch(List<LoadDTO> loadDTOList, String search) {
+        LoadDTO loadDTO;
+        List<WithoutContent> searchWithStackList = employmentRepository.findAllByStackLike(search);
+
+        for (WithoutContent stackSearch : searchWithStackList) {
+            loadDTO = new LoadDTO(stackSearch);
+            loadDTOList.add(loadDTO);
+        }
     }
 }
