ffeb500387b67ec6415166b5f96aeae07ad31a8e
jojeongbin <cjb1009@naver.com>
2022-12-25 21:53:26 +0900

    REFACTOR(EmploymentController) :  Change parameter

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -1,4 +1,29 @@
 package com.example.backend.controller;
 
+import com.example.backend.DTO.EmploymentDeleteDTO;
+import com.example.backend.entity.Company;
+import com.example.backend.entity.Employment;
+import com.example.backend.service.EmploymentService;
+import com.sun.xml.bind.v2.model.core.ID;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
 public class EmploymentController {
+    private  final EmploymentService employmentService;
+
+    public EmploymentController(EmploymentService boardService) {
+        this.employmentService = boardService;
+    }
+
+    @GetMapping("/employ/delete")
+    public void employDelete(@RequestParam Long id){
+        employmentService.employDelete(id);
+    }
+
 }
