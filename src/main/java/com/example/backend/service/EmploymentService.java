df8f1aff2eb43aed61d67fd08a64c3d41b40511e
jojeongbin <cjb1009@naver.com>
2022-12-25 23:31:17 +0900

    REFACTOR(EmploymentRepository) : Change nothing

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -1,4 +1,25 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.EmploymentDeleteDTO;
+import com.example.backend.REPO.CompanyRepository;
+import com.example.backend.REPO.EmploymentRepository;
+import com.example.backend.VO.EmploymentDeleteVO;
+import com.example.backend.entity.Company;
+import com.example.backend.entity.Employment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class EmploymentService {
+    private final EmploymentRepository employmentRepository;
+    private final CompanyRepository companyRepository;
+
+    @Transactional
+    public String employDelete(Long id)  {
+      EmploymentRepository.delete();
+
 }
