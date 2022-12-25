a5cd9ca5778fa7b6244f8994cb01234aa1920cfe
dong yeol <ehfql6363@naver.com>
2022-12-25 12:53:22 +0900

    FEAT(EmploymentDetailService) : Change return type and create method
    Signed-off-by: dong yeol <ehfql6363@naver.com>

--- src/main/java/com/example/backend/service/EmploymentDetailService.java
+++ src/main/java/com/example/backend/service/EmploymentDetailService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class CompanyService {
+public class EmploymentDetailService {
 
     private final CompanyRepository companyRepository;
     private final EmploymentRepository employmentRepository;
