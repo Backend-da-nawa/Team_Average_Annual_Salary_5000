31f75b7e056c15270ba56663757f5ecf6b88ecd9
jojeongbin <cjb1009@naver.com>
2022-12-25 23:24:50 +0900

    FEAT(EmploymentController) : Change annotation

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -1,7 +1,21 @@
 package com.example.backend.controller;
 
+import com.example.backend.service.EmploymentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class EmploymentController {
+    private  final EmploymentService employmentService;
+
+    public EmploymentController(EmploymentService boardService) {
+        this.employmentService = boardService;
+    }
+    @GetMapping("/employ/modify/{id}")
+    public void employModify(@PathVariable("id") Integer id, Model model){
+
+    }
+
 }
