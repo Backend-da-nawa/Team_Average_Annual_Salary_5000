38206f0521fff4cb1ff6679b85141dcc64a6d309
jojeongbin <cjb1009@naver.com>
2022-12-26 01:13:29 +0900

    FEAT(EmploymentService) : Create optional and builder ,Change parameter

--- src/main/java/com/example/backend/service/EmploymentService.java
+++ src/main/java/com/example/backend/service/EmploymentService.java
@@ -1,7 +1,42 @@
 package com.example.backend.service;
 
+import com.example.backend.DTO.EmploymentModifyDTO;
+import com.example.backend.REPO.CompanyRepository;
+import com.example.backend.REPO.EmploymentRepository;
+import com.example.backend.entity.Company;
+import com.example.backend.entity.Employment;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class EmploymentService {
+    private final EmploymentRepository employmentRepository;
+    private final CompanyRepository companyRepository;
+
+    public Long employModify(Map<String,Object> map) throws Exception {
+        Long comId = Long.parseLong((String) map.get("comId"));
+        Optional<Company> optional= companyRepository.findById( Long.parseLong((String) map.get("comId")));
+        Company company;
+        if(optional.isPresent()) company=optional.get();
+        else  throw  new Exception();
+
+        EmploymentModifyDTO employmentModifyDTO = EmploymentModifyDTO.builder()
+                .id(Long.parseLong((String) map.get("empId")))
+                .company(company)
+                .compensation(Long.parseLong((String) map.get("compensation")))
+                .position((String) map.get("position"))
+                .stack((String) map.get("stack"))
+                .content((String) map.get("content"))
+                .build();
+        Employment employment= Employment.builder().employmentModifyDTO(employmentModifyDTO).build();
+        employmentRepository.save(employment);
+        return 1L;
+    }
+
+
+
 }
