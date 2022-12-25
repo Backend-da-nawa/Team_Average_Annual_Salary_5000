05353ef372900d2bdd59a2d268a9b8bd5b896d3c
jojeongbin <cjb1009@naver.com>
2022-12-26 01:09:21 +0900

    FEAT(EmploymentController) : Change parameter

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -3,18 +3,21 @@ package com.example.backend.controller;
 import com.example.backend.service.EmploymentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
 public class EmploymentController {
     private  final EmploymentService employmentService;
 
     public EmploymentController(EmploymentService boardService) {
         this.employmentService = boardService;
     }
-    @GetMapping("/employ/modify/{id}")
-    public void employModify(@PathVariable("id") Integer id, Model model){
+    @GetMapping("/employ/modify")
+    public Long employModify(@RequestBody Map<String,Object> map)throws Exception{
+
+        return employmentService.employModify(map);
 
     }
 
