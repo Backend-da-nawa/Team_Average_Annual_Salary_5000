3a3801dc8c0cc8851b2c403a77318e1ad49cc85a
dong yeol <ehfql6363@naver.com>
2023-01-02 00:47:33 +0900

    REFATOR(EmploymentController) : EmploymentController 줄 간격 등 수정

--- src/main/java/com/example/backend/controller/EmploymentController.java
+++ src/main/java/com/example/backend/controller/EmploymentController.java
@@ -20,21 +20,18 @@ public class EmploymentController {
     private final EmploymentService employmentService;
 
     @GetMapping("/employ/modify")
-    public Long employModify(@RequestBody Map<String,Object> map)throws Exception{
-
+    public Long employModify(@RequestBody Map<String, Object> map) throws Exception {
         return employmentService.employModify(map);
-
     }
 
 
-    @GetMapping("/employ/delete")
+    @DeleteMapping("/employ/delete")
     public Long employDelete(@RequestParam Long empId){
-
         return employmentService.employDelete(empId);
     }
 
     @PostMapping("/employ/submit")
-    public void employSubmit(@RequestBody EmploymentSubmitVO employmentSubmitVO)throws Exception{
+    public void employSubmit(@RequestBody EmploymentSubmitVO employmentSubmitVO) throws Exception {
         employmentService.employSubmit(employmentSubmitVO);
     }
 
