8aa56f6b5b540ad6005b5c65aab0f4f31fe1c195
dong yeol <ehfql6363@naver.com>
2023-01-02 13:29:43 +0900

    REFACTOR(LoadEmpTest) : 테스트 케이스 클래스 이름 변경

--- src/test/java/com/example/backend/featureTest/LoadEmpTest.java
+++ src/test/java/com/example/backend/featureTest/LoadEmpTest.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,7 +25,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("채용 공고 테스트")
-public class loadEmpTest {
+public class LoadEmpTest {
 
     @LocalServerPort
     private int port;
